d5c142f586b04e5ce45c33454b2b86861634b815
ArhumJ <arhumjain10@gmail.com>
2020-12-29 18:43:15 -0800

    add sidebar view

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
+// this method is called when your extension is activatedm
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,17 +10,31 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "theme-editor" is now active!');
 
+    vscode.extensions.all.forEach(ext => {
+        const contributesThemes = ext.packageJSON.contributes ? (ext.packageJSON.contributes.themes ? ext.packageJSON.contributes.themes : undefined) : undefined;
+        if (contributesThemes) {
+            for (var i = 0; i < contributesThemes.length; i++) {
+                const label = contributesThemes[i].label;
+                const uiTheme = (contributesThemes[i].uiTheme === 'vs-dark') ? 'dark' : 'light';
+                const extensionType = ext.packageJSON.isBuiltin ? 'Built-in' : 'External';
+                vscode.window.showInformationMessage(`${extensionType} extension '${ext.id}' contributes ${uiTheme} theme '${label}'`);
+             }
+        }
+    });
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('theme-editor.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-
 		// Display a message box to the user
+	vscode.window.showInformationMessage(vscode.extensions.all[0].packageJSON.contributes.themes.id)
 		vscode.window.showInformationMessage('Hello World from Theme Editor!');
 	});
 
 	context.subscriptions.push(disposable);
+}
+function commentLint(){
+	vscode.commands.executeCommand('theme-editor.helloWorld')
 }
 
 // this method is called when your extension is deactivated
